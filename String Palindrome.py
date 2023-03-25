ce665111227618a15a9fbc26148073af1acf756a
PraveenKumar854 <128894048+PraveenKumar854@users.noreply.github.com>
2023-03-25 14:24:51 +0530

    Add files via upload

---
+++ String Palindrome.py
@@ -0,0 +1,6 @@
+n=input("Enter the String:")
+m=n[::-1]
+if n==m:
+    print(n,"is a Palindrome")
+else:
+    print(n,"is not a Palindrome")
---
+++ Swapping Numbers.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter 1st Number:"))
+num2 = int(input("Enter 2nd Number:"))
+print ("Before swapping: ")
+print("Value of num1 : ", num1, " and num2 : ", num2)
+num1, num2 = num2, num1
+print ("After swapping: ")
+print("Value of num1 : ", num1, " and num2 : ", num2)
